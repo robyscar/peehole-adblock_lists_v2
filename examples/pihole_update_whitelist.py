65039369cc4fbcd4e64b7a61a853fe9b97985210
robyscar <robyscar@users.noreply.github.com>
2022-08-16 21:14:41 +0700

    .

--- examples/pihole_update_whitelist.py
+++ examples/pihole_update_whitelist.py
@@ -7,21 +7,6 @@
 # For an example of how to deploy this script, see 
 # See https://www.bentasker.co.uk/posts/documentation/general/refreshing-piholes-regex-block-list-from-external-sources.html#installation
 #
-'''
-License: BSD 3-Clause
-
-Copyright 2022 B Tasker
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
 
 import hashlib
 import os
@@ -54,6 +39,7 @@ def writeEntries(domain_list,comment):
     conn.commit()
     
 # Fetch the whitelist
+### whitelist = fetchList('https://raw.githubusercontent.com/bentasker/adblock_lists_v2/master/lists/alloweddomains.txt')
 whitelist = fetchList('https://raw.githubusercontent.com/bentasker/adblock_lists_v2/master/lists/alloweddomains.txt')
 
 # Remove empty lines and sort
@@ -83,6 +69,7 @@ if os.path.exists("/etc/pihole/allow_list_cache"):
     
 # Has the list changed?
 if cache_sha1 != merged_sha:
+###    writeEntries(whitelist, 'bentasker/adblock_lists_v2/allowlist')
     writeEntries(whitelist, 'bentasker/adblock_lists_v2/allowlist')
     fh = open("/etc/pihole/allow_list_cache", "w")
     fh.write(mergedstr)
